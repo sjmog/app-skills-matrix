bb7b972d286643b924b78f222be3dbc3ef581095
Charlie Harris <ceharris92@gmail.com>
2017-02-13 20:53:11 +0000

    Add auth middleware to templates and skills POST endpoints

--- backend/routes/matrices.js
+++ backend/routes/matrices.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
+
+const { ensureAdmin } = require('../middlewares/auth');
 const { templates, skills } = require('../handlers/matrices');
 
 module.exports = (app) => {
   const router = Router();
 
-  router.post('/templates', (req, res, next) => templates[req.body.action](req, res, next));
-  router.post('/skills', (req, res, next) => skills[req.body.action](req, res, next));
+  router.post('/templates', ensureAdmin, (req, res, next) => templates[req.body.action](req, res, next));
+  router.post('/skills', ensureAdmin, (req, res, next) => skills[req.body.action](req, res, next));
 
   app.use('/', router);
 
