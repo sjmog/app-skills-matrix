4c284145f6fdbbf1aed5eded9ef86a33b9155834
David Morgantini <dmorgantini@gmail.com>
2017-03-17 09:55:15 +0000

    Remove duplicated line

--- backend/routes/evaluations.js
+++ backend/routes/evaluations.js
@@ -6,7 +6,6 @@ module.exports = (app) => {
   const router = Router();
 
   router.post('/:evaluationId', (req,res, next) => evaluation[req.body.action](req, res, next));
-  router.post('/:evaluationId', (req,res, next) => evaluation[req.body.action](req, res, next));
   router.get('/:evaluationId', (req,res, next) => evaluation.retrieve(req, res, next));
   app.use('/evaluations', router);
 
