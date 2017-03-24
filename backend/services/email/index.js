662b1891bb7084fbe1966675059a7c7f4c6b0a07
dmorgantini <dmorgantini@gmail.com>
2017-03-24 22:53:42 +0000

    add ses module (doesn't work cause config is broken)

--- backend/services/email/index.js
+++ backend/services/email/index.js
@@ -1,9 +1,11 @@
 const mailgun = require('./mailgun');
+const ses = require('./ses');
 
 const MAIL_PROVIDER = process.env.MAIL_PROVIDER;
 
 const providers = {
   mailgun: mailgun.sendMail,
+  ses: ses.sendMail,
 };
 
 module.exports.sendMail = ({ recipients, subject, body }) => {
