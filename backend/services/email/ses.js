d202d4114f6bd287fae14c7709572607f5eecb6f
dmorgantini <dmorgantini@gmail.com>
2017-04-04 13:48:35 +0100

    changes to allow ses to work with local credentials

--- backend/services/email/ses.js
+++ backend/services/email/ses.js
@@ -4,18 +4,26 @@ const Promise = require('bluebird');
 const sesRegion = process.env.SES_REGION;
 const roleArn = process.env.SES_ROLE_ARN;
 const roleSessionName = process.env.SES_SESSION_NAME;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 
 const getSES = function getSES() {
-  const sesConfig = { region: sesRegion };
-  const stsConfig = {
-    RoleArn: roleArn,
-    RoleSessionName: roleSessionName,
-    DurationSeconds: 900,
-  };
+  if (!accessKeyId && !roleArn) {
+    throw new Error('Credentials must be provided in either process.env.SES_ROLE_ARN or process.env.AWS_ACCESS_KEY_ID');
+  }
 
+  const sesConfig = { region: sesRegion };
   AWS.config.update(sesConfig);
-  AWS.config.credentials = new AWS.EC2MetadataCredentials();
-  AWS.config.credentials = new AWS.TemporaryCredentials(stsConfig);
+
+  if (!accessKeyId) {
+    const stsConfig = {
+      RoleArn: roleArn,
+      RoleSessionName: roleSessionName,
+      DurationSeconds: 900,
+    };
+
+    AWS.config.credentials = new AWS.EC2MetadataCredentials();
+    AWS.config.credentials = new AWS.TemporaryCredentials(stsConfig);
+  }
 
   return new AWS.SES();
 };
