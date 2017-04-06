641bd1e1e14574bb8c871d0549bec1fd81de833b
dmorgantini <dmorgantini@gmail.com>
2017-04-06 12:01:57 +0100

    remove configuration as not currently needed

--- backend/services/email/ses.js
+++ backend/services/email/ses.js
@@ -2,28 +2,10 @@ const AWS = require('aws-sdk');
 const Promise = require('bluebird');
 
 const sesRegion = process.env.SES_REGION;
-const roleArn = process.env.SES_ROLE_ARN;
-const roleSessionName = process.env.SES_SESSION_NAME;
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 
 const getSES = function getSES() {
-  if (!accessKeyId && !roleArn) {
-    throw new Error('Credentials must be provided in either process.env.SES_ROLE_ARN or process.env.AWS_ACCESS_KEY_ID');
-  }
-
   const sesConfig = { region: sesRegion };
   AWS.config.update(sesConfig);
-
-  if (!accessKeyId) {
-    const stsConfig = {
-      RoleArn: roleArn,
-      RoleSessionName: roleSessionName,
-      DurationSeconds: 900,
-    };
-
-    AWS.config.credentials = new AWS.EC2MetadataCredentials();
-    AWS.config.credentials = new AWS.TemporaryCredentials(stsConfig);
-  }
 
   return new AWS.SES();
 };
