f44e9458527818012197533b37f94294d6698070
dmorgantini <dmorgantini@gmail.com>
2017-04-21 13:43:18 +0100

    fixed thing that would tell Jake off

--- backend/routes/evaluations.js
+++ backend/routes/evaluations.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
 
   router.post('/:evaluationId', (req,res, next) => evaluation[req.body.action](req, res, next));
   router.get('/:evaluationId', (req,res, next) => evaluation.retrieve(req, res, next));
-  router.post('/', (req, res, next) => evaluations[req.body.action](req, res, next));
+  router.post('/', (req, res, next) => evaluations.import(req, res, next));
   app.use('/evaluations', router);
 
   return app
