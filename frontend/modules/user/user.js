f510a786b1953bda3152b472ee08a0b9c2c5913b
Charlie Harris <ceharris92@gmail.com>
2017-04-22 16:07:07 +0100

    Header now displays logged in username

--- frontend/modules/user/user.js
+++ frontend/modules/user/user.js
@@ -1,5 +1,9 @@
 import { handleActions } from 'redux-actions';
+import R from 'ramda';
 
 export default handleActions({
   foo: (state) => state, // :-/ needed cause reasons (redux??)
 }, { userDetails: {} });
+
+export const getUsername = (state) =>
+  R.path(['userDetails', 'username'], state);
