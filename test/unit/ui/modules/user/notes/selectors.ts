fe75a1034643ce55e68977bf82683ebf6815b00e
Charlie Harris <ceharris92@gmail.com>
2017-09-03 18:13:00 +0100

    Change notes selectors test file from JS to TS

--- test/unit/ui/modules/user/notes/selectors.ts
+++ test/unit/ui/modules/user/notes/selectors.ts
@@ -1,5 +1,5 @@
-import {expect} from 'chai';
-import {getSortedNotes } from '../../../../../../frontend/modules/user/notes';
+import { expect } from 'chai';
+import { getSortedNotes } from '../../../../../../frontend/modules/user/notes';
 
 describe('Notes selectors', () => {
   describe('getSortedNotes', () => {
@@ -9,7 +9,7 @@ describe('Notes selectors', () => {
           NOTE_ID_1: { createdDate: '1900-01-01T00:01:01.001Z' },
           NOTE_ID_2: { createdDate: '2000-01-01T00:01:01.001Z' },
           NOTE_ID_3: { createdDate: '1950-01-01T00:01:01.001Z' },
-        }
+        },
       };
 
       expect(getSortedNotes(state, ['NOTE_ID_1', 'NOTE_ID_2', 'NOTE_ID_3'])).to.eql([
@@ -25,7 +25,7 @@ describe('Notes selectors', () => {
           NOTE_ID_1: { createdDate: '1900-01-01T00:01:01.001Z' },
           NOTE_ID_2: { createdDate: '2000-01-01T00:01:01.001Z' },
           NOTE_ID_3: { createdDate: '1950-01-01T00:01:01.001Z' },
-        }
+        },
       };
 
       expect(getSortedNotes(state, ['NOTE_ID_1', 'NOTE_ID_2'])).to.eql([
@@ -40,13 +40,13 @@ describe('Notes selectors', () => {
           NOTE_ID_1: { createdDate: '1900-01-01T00:01:01.001Z' },
           NOTE_ID_2: { createdDate: true },
           NOTE_ID_3: { createdDate: null },
-        }
+        },
       };
 
       expect(getSortedNotes(state, ['NOTE_ID_1', 'NOTE_ID_2', 'NOTE_ID_3'])).to.eql([
         { createdDate: '1900-01-01T00:01:01.001Z' },
         { createdDate: true },
-        { createdDate: null }
+        { createdDate: null },
       ]);
     });
 
@@ -54,7 +54,7 @@ describe('Notes selectors', () => {
       const state = {
         entities: {
           NOTE_ID_1: { createdDate: '1900-01-01T00:01:01.001Z' },
-        }
+        },
       };
 
       expect(getSortedNotes(state, ['NOTE_ID_1', 'NOTE_ID_2'])).to.eql([
@@ -67,39 +67,18 @@ describe('Notes selectors', () => {
       const state = {
         entities: {
           NOTE_ID_1: { createdDate: '1900-01-01T00:01:01.001Z' },
-        }
+        },
       };
 
       expect(getSortedNotes(state, [])).to.eql([]);
-    });
-
-    it('returns an empty list when the list of notes in the request is invalid', () => {
-      const state = {
-        entities: {
-          NOTE_ID_1: { createdDate: '1900-01-01T00:01:01.001Z' },
-        }
-      };
-
-      expect(getSortedNotes(state, 'INVALID')).to.eql([]);
-    });
-
-    it('handles invalid note ids', () => {
-      const state = {
-        entities: {
-          NOTE_ID_1: { createdDate: '1900-01-01T00:01:01.001Z' },
-        }
-      };
-      expect(getSortedNotes(state, [{}, '', 0, false, true, [1], 'NOTE_ID_1'])).to.eql([
-        null, null, null, null, null, null, { createdDate: '1900-01-01T00:01:01.001Z' }
-      ]);
     });
 
     it('handles notes with no createdDate', () => {
       const state = {
         entities: {
           NOTE_ID_1: {},
-          NOTE_ID_2: { createdDate: '1900-01-01T00:01:01.001Z' }
-        }
+          NOTE_ID_2: { createdDate: '1900-01-01T00:01:01.001Z' },
+        },
       };
       expect(getSortedNotes(state, ['NOTE_ID_1', 'NOTE_ID_2'])).to.eql([
         {},
